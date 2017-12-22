daaa0edfc6af1d24329c15280b3ee0ef268ebcaa
@louisdelosr <ll_reborn_dr@hotmail.com>
2017-12-22 09:36:30 -0500

    Add files via upload

---
+++ js/mobilemenu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("all").style.overflow = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("all").style.overflow = "visible";
+}
